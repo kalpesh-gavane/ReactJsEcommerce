bc9a33cfebbd70b388aedecf241b2e47854746c8
Kalpesh <kalpesh@beepixl.com>
2022-05-06 23:30:36 +0530

    filter by category and price in product grid page

--- src/components/products.js
+++ src/components/products.js
@@ -4,7 +4,7 @@ export const products = [
         "product_name": "Parle Krack Jack 34.36G Pack Of 5",
         "order_number": null,
         "image": "16265202771.DD.jpg",
-        "mrp": 50,
+        "mrp": 15,
         "mmd_price": 46,
         "quantity": -1,
         "active": 1,
@@ -40,7 +40,7 @@ export const products = [
         "product_sgst_per": null,
 
         "product_igst_per": null,
-        "product_cat_id": 86,
+        "product_cat_id": 43,
         "product_sub_cat_id": 172,
 
         "brand_id": null,
@@ -56,7 +56,7 @@ export const products = [
         "voteCount": 0,
         "order_number": null,
         "image": "16265205921.DD.jpg",
-        "mrp": 50,
+        "mrp": 120,
         "mmd_price": 46,
         "quantity": 1,
         "active": 1,
@@ -92,7 +92,371 @@ export const products = [
         "product_sgst_per": null,
 
         "product_igst_per": null,
-        "product_cat_id": 86,
+        "product_cat_id": 't-shirt',
+        "product_sub_cat_id": 172,
+
+        "brand_id": null,
+        "created_at": "2021-07-17 16:46:32",
+        "updated_at": "2021-12-07 19:30:25",
+
+        "deleted_at": null
+    },
+
+    {
+        "product_id": 1707,
+        "product_name": "Parle Monaco Classic 66.7G Pack Of 5",
+        "voteCount": 0,
+        "order_number": null,
+        "image": "16265205921.DD.jpg",
+        "mrp": 200,
+        "mmd_price": 46,
+        "quantity": 1,
+        "active": 1,
+        "approved": 1,
+        "min_qty": 1,
+        "max_qty": 5,
+        "size": "334 GM",
+
+        "featured": null,
+
+        "low_price": null,
+
+        "best_deals": null,
+        "discount": 8,
+        "points": 46,
+        "credit_points": 1,
+        "vendor_id": 26,
+        "city_id": 1,
+
+        "b2b_qty": null,
+
+        "b2b_price": null,
+        "whole_sale_price": 46,
+
+        "product_cgst_per": null,
+
+        "product_sgst_amt": null,
+
+        "product_igst_amt": null,
+
+        "product_cgst_amt": null,
+
+        "product_sgst_per": null,
+
+        "product_igst_per": null,
+        "product_cat_id": 'men',
+        "product_sub_cat_id": 172,
+
+        "brand_id": null,
+        "created_at": "2021-07-17 16:46:32",
+        "updated_at": "2021-12-07 19:30:25",
+
+        "deleted_at": null
+    },
+
+    {
+        "product_id": 1707,
+        "product_name": "Parle Monaco Classic 66.7G Pack Of 5",
+        "voteCount": 0,
+        "order_number": null,
+        "image": "16265205921.DD.jpg",
+        "mrp": 0,
+        "mmd_price": 46,
+        "quantity": 1,
+        "active": 1,
+        "approved": 1,
+        "min_qty": 1,
+        "max_qty": 5,
+        "size": "334 GM",
+
+        "featured": null,
+
+        "low_price": null,
+
+        "best_deals": null,
+        "discount": 8,
+        "points": 46,
+        "credit_points": 1,
+        "vendor_id": 26,
+        "city_id": 1,
+
+        "b2b_qty": null,
+
+        "b2b_price": null,
+        "whole_sale_price": 46,
+
+        "product_cgst_per": null,
+
+        "product_sgst_amt": null,
+
+        "product_igst_amt": null,
+
+        "product_cgst_amt": null,
+
+        "product_sgst_per": null,
+
+        "product_igst_per": null,
+        "product_cat_id": 'men',
+        "product_sub_cat_id": 172,
+
+        "brand_id": null,
+        "created_at": "2021-07-17 16:46:32",
+        "updated_at": "2021-12-07 19:30:25",
+
+        "deleted_at": null
+    },
+
+    {
+        "product_id": 1707,
+        "product_name": "Parle Monaco Classic 66.7G Pack Of 5",
+        "voteCount": 0,
+        "order_number": null,
+        "image": "16265205921.DD.jpg",
+        "mrp": 45,
+        "mmd_price": 46,
+        "quantity": 1,
+        "active": 1,
+        "approved": 1,
+        "min_qty": 1,
+        "max_qty": 5,
+        "size": "334 GM",
+
+        "featured": null,
+
+        "low_price": null,
+
+        "best_deals": null,
+        "discount": 8,
+        "points": 46,
+        "credit_points": 1,
+        "vendor_id": 26,
+        "city_id": 1,
+
+        "b2b_qty": null,
+
+        "b2b_price": null,
+        "whole_sale_price": 46,
+
+        "product_cgst_per": null,
+
+        "product_sgst_amt": null,
+
+        "product_igst_amt": null,
+
+        "product_cgst_amt": null,
+
+        "product_sgst_per": null,
+
+        "product_igst_per": null,
+        "product_cat_id": 'women',
+        "product_sub_cat_id": 172,
+
+        "brand_id": null,
+        "created_at": "2021-07-17 16:46:32",
+        "updated_at": "2021-12-07 19:30:25",
+
+        "deleted_at": null
+    },
+
+    {
+        "product_id": 1707,
+        "product_name": "Parle Monaco Classic 66.7G Pack Of 5",
+        "voteCount": 0,
+        "order_number": null,
+        "image": "16265205921.DD.jpg",
+        "mrp": 89,
+        "mmd_price": 46,
+        "quantity": 1,
+        "active": 1,
+        "approved": 1,
+        "min_qty": 1,
+        "max_qty": 5,
+        "size": "334 GM",
+
+        "featured": null,
+
+        "low_price": null,
+
+        "best_deals": null,
+        "discount": 8,
+        "points": 46,
+        "credit_points": 1,
+        "vendor_id": 26,
+        "city_id": 1,
+
+        "b2b_qty": null,
+
+        "b2b_price": null,
+        "whole_sale_price": 46,
+
+        "product_cgst_per": null,
+
+        "product_sgst_amt": null,
+
+        "product_igst_amt": null,
+
+        "product_cgst_amt": null,
+
+        "product_sgst_per": null,
+
+        "product_igst_per": null,
+        "product_cat_id": 'girl',
+        "product_sub_cat_id": 172,
+
+        "brand_id": null,
+        "created_at": "2021-07-17 16:46:32",
+        "updated_at": "2021-12-07 19:30:25",
+
+        "deleted_at": null
+    },
+
+    {
+        "product_id": 1707,
+        "product_name": "Parle Monaco Classic 66.7G Pack Of 5",
+        "voteCount": 0,
+        "order_number": null,
+        "image": "16265205921.DD.jpg",
+        "mrp": 65,
+        "mmd_price": 46,
+        "quantity": 1,
+        "active": 1,
+        "approved": 1,
+        "min_qty": 1,
+        "max_qty": 5,
+        "size": "334 GM",
+
+        "featured": null,
+
+        "low_price": null,
+
+        "best_deals": null,
+        "discount": 8,
+        "points": 46,
+        "credit_points": 1,
+        "vendor_id": 26,
+        "city_id": 1,
+
+        "b2b_qty": null,
+
+        "b2b_price": null,
+        "whole_sale_price": 46,
+
+        "product_cgst_per": null,
+
+        "product_sgst_amt": null,
+
+        "product_igst_amt": null,
+
+        "product_cgst_amt": null,
+
+        "product_sgst_per": null,
+
+        "product_igst_per": null,
+        "product_cat_id": 'girl',
+        "product_sub_cat_id": 172,
+
+        "brand_id": null,
+        "created_at": "2021-07-17 16:46:32",
+        "updated_at": "2021-12-07 19:30:25",
+
+        "deleted_at": null
+    },
+
+    {
+        "product_id": 1707,
+        "product_name": "Parle Monaco Classic 66.7G Pack Of 5",
+        "voteCount": 0,
+        "order_number": null,
+        "image": "16265205921.DD.jpg",
+        "mrp": 12,
+        "mmd_price": 46,
+        "quantity": 1,
+        "active": 1,
+        "approved": 1,
+        "min_qty": 1,
+        "max_qty": 5,
+        "size": "334 GM",
+
+        "featured": null,
+
+        "low_price": null,
+
+        "best_deals": null,
+        "discount": 8,
+        "points": 46,
+        "credit_points": 1,
+        "vendor_id": 26,
+        "city_id": 1,
+
+        "b2b_qty": null,
+
+        "b2b_price": null,
+        "whole_sale_price": 46,
+
+        "product_cgst_per": null,
+
+        "product_sgst_amt": null,
+
+        "product_igst_amt": null,
+
+        "product_cgst_amt": null,
+
+        "product_sgst_per": null,
+
+        "product_igst_per": null,
+        "product_cat_id": 'boy',
+        "product_sub_cat_id": 172,
+
+        "brand_id": null,
+        "created_at": "2021-07-17 16:46:32",
+        "updated_at": "2021-12-07 19:30:25",
+
+        "deleted_at": null
+    },
+
+    {
+        "product_id": 1707,
+        "product_name": "Parle Monaco Classic 66.7G Pack Of 5",
+        "voteCount": 0,
+        "order_number": null,
+        "image": "16265205921.DD.jpg",
+        "mrp": 70,
+        "mmd_price": 46,
+        "quantity": 1,
+        "active": 1,
+        "approved": 1,
+        "min_qty": 1,
+        "max_qty": 5,
+        "size": "334 GM",
+
+        "featured": null,
+
+        "low_price": null,
+
+        "best_deals": null,
+        "discount": 8,
+        "points": 46,
+        "credit_points": 1,
+        "vendor_id": 26,
+        "city_id": 1,
+
+        "b2b_qty": null,
+
+        "b2b_price": null,
+        "whole_sale_price": 46,
+
+        "product_cgst_per": null,
+
+        "product_sgst_amt": null,
+
+        "product_igst_amt": null,
+
+        "product_cgst_amt": null,
+
+        "product_sgst_per": null,
+
+        "product_igst_per": null,
+        "product_cat_id": 'boy',
         "product_sub_cat_id": 172,
 
         "brand_id": null,
